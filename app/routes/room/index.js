0614e417ce2f74be53a260cfcb616fc3d09191cb
Tiso Alvarez Puccinelli <tiso_ap@unseen.is>
2017-11-19 11:33:05 -0200

    Add debugger

--- app/routes/room/index.js
+++ app/routes/room/index.js
@@ -1,14 +1,17 @@
 const {Router} = require('express')
+const debug = require('debug')('typeracer')
 
 const router = Router()
 
 router.get('/:roomname/user/:username', (request, response) => {
   const {roomname, username} = request.params
+  debug(`Main room route with room "${roomname}" and user "${username}"`)
   response.render('index', {roomname, username})
 })
 
 router.get('/:roomname/status', (request, response) => {
   const {roomname} = request.params
+  debug(`Room status route with room "${roomname}"`)
   response.send({roomname})
 })
 
