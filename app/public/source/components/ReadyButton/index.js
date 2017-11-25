a077007fc36446902b14d89b9be220b4c88c9fcc
Tiso Alvarez Puccinelli <tiso_ap@unseen.is>
2017-11-25 18:23:14 -0200

    Simpler Ready button

--- app/public/source/components/ReadyButton/index.js
+++ app/public/source/components/ReadyButton/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 
 const ReadyButton = ({ready, onClick}) => {
-  if (!ready) {
-    return <button
-      className='button button-primary'
-      onClick={onClick}>I am ready!</button>
-  } else {
-    return null
-  }
+  if (ready) return null
+  return <button
+    className='button button-primary'
+    onClick={onClick}>I am ready!</button>
 }
 
 export default ReadyButton
